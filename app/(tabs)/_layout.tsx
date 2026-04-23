c272211f36996aa01ef33bb7bc82bbecb3493c6e
Mateus Peres <mateuspereslopesl@outlook.com>
2026-04-23 17:57:46 -0300

    adicionando tela de perfil

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,6 +44,16 @@ export default function TabLayout() {
           ),
         }}
       />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="person.fill" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
