d9466773256696849fb3812de90b2b9d95f3657f
Mateus Peres <mateuspereslopesl@outlook.com>
2026-04-27 11:46:06 -0300

    ajustando o hashing de senha do usuário

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,7 +1,26 @@
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => value.trim())
   username!: string;
+
+  @IsString()
+  @IsNotEmpty()
   first_name!: string;
+
+  @IsString()
+  @IsNotEmpty()
   last_name!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => value.toLowerCase())
   email!: string;
+
+  @IsString()
+  @MinLength(8)
   pass!: string;
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -6,9 +7,14 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
   async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.pass, 10);
     return this.prisma.user.create({
-      data: createUserDto,
+      data: {
+        ...createUserDto,
+        pass: hashedPassword,
+      },
     });
   }
 
@@ -23,6 +29,10 @@ export class UsersService {
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.pass) {
+      updateUserDto.pass = await bcrypt.hash(updateUserDto.pass, 10);
+    }
+
     return this.prisma.user.update({
       where: { id },
       data: updateUserDto,
