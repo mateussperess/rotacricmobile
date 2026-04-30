697c94d22b3defb7d2f857605425ad9307c40823
Mateus Peres <mateuspereslopesl@outlook.com>
2026-04-30 00:12:21 -0300

    criando conta via api para o backend via mobile app + login (apenas servidor por enquanto)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://meu_ip:3000",
+  baseURL: `http://${process.env.EXPO_PUBLIC_IP}:3000`, // definir o ip na .env da raiz
 });
 
 export default api;
