85ce4a126dcf9c1efbf786a0ffd54c47e3eead9b
mateussperess <mateuspereslopesl@outlook.com>
2026-05-29 18:51:10 -0300

    feat: listagem basica inicial das cidades na aba cidades

--- services/cities/citiesService.ts
+++ services/cities/citiesService.ts
@@ -25,4 +25,14 @@ export const CitiesService = {
       return null;
     }
   },
+
+  findAll: async (): Promise<City[] | null> => {
+    try {
+      const { data } = await api.get("/cities");
+      return data;
+    } catch (error) {
+      console.error("Error fetching cities:", error);
+      return null;
+    }
+  },
 };
