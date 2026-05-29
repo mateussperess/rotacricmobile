170a87a32e98da274075e65d7394731ca3c67a8c
mateussperess <mateuspereslopesl@outlook.com>
2026-05-29 18:15:42 -0300

    fix: removido a rota map em definitivo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -46,16 +46,6 @@ export default function TabLayout() {
       />
 
       <Tabs.Screen
-        name="map"
-        options={{
-          title: "Mapa Web",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="map.fill" color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
         name="cidades"
         options={{
           title: "Cidades",
